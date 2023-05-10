4184eca30c60f6a02d761c79b9c749abd88fa5f0
bayurkp <bayurkp194@gmail.com>
2023-05-10 22:08:24 +0800

    Membuat method untuk melakukan parsing terhadap JSON string yang dikirimkan oleh user

--- src/main/java/com/bay/server/Request.java
+++ src/main/java/com/bay/server/Request.java
@@ -1,9 +1,14 @@
 package com.bay.server;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class Request {
     public static class Handler implements HttpHandler {
@@ -13,10 +18,18 @@ public class Request {
                     exchange.getRequestMethod().equals("POST") ||
                     exchange.getRequestMethod().equals("PUT") ||
                     exchange.getRequestMethod().equals("DELETE")) {
-                new Response(exchange, 200, "{\"statusCode\": 200, \"message\": \"Hello "  + exchange.getRequestMethod() + "\"}");
+                new Response(exchange, 200, "{\"statusCode\": 200, " + "\"message\": \"Hello, " + parseRequestBody(exchange).get("name").asText() + "!\"}");
             } else {
                 new Response(exchange, 405, "{\"statusCode\": 405, \"message\": \"Method not allowed\"}");
             }
         }
+    }
+
+    public static JsonNode parseRequestBody(HttpExchange exchange) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        InputStream requestBody = exchange.getRequestBody();
+        Scanner scanner = new Scanner(requestBody).useDelimiter("\\A");
+        String result = scanner.hasNext() ? scanner.next() : "";
+        return objectMapper.readTree(result);
     }
 }
--- src/main/java/com/bay/server/Response.java
+++ src/main/java/com/bay/server/Response.java
@@ -12,6 +12,5 @@ public class Response {
         outputStream.write(jsonMessage.getBytes());
         outputStream.flush();
         outputStream.close();
-
     }
 }
